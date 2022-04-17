3fb222ebeb16fe9b8ae2d3fc3d1c56a441d2a482
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-04-18 00:48:55 +0900

    [Bronze III] Title: N 찍기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+
+int main() {
+    int N;
+    std::cin >> N;
+    
+    
+    for (int i = 1;i <= N;++i) {
+            std::cout <<i<<"\n";
+    }
+   
+   
+   
+	return 0;
+}
