26ddc4ea7439c44d72ce0cecebf7cf2f1688ec5f
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-05-18 02:00:37 +0900

    [Silver II] Title: 최소 힙, Time: 20 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,35 @@
+#include <cstdio>
+#include <queue>
+#include <iostream>  
+#include <functional>
+#pragma warning(disable: 4996) 
+using namespace std;
+priority_queue<int, vector<int>, greater<int> > pq;
+int N;
+
+int main() {
+    scanf("%d", &N);
+    for (int n_idx = 0; n_idx < N; n_idx++) {
+        int _input_int;
+        scanf("%d", &_input_int);
+
+        // 0이면 하나 빼서 출력
+        // 아니면 삽입 
+        switch (_input_int) {
+        case 0:
+            if (pq.size()){
+                printf("%d\n", pq.top());
+                pq.pop();
+            }
+            else {
+                printf("%d\n", 0);
+            }
+            break;
+        default:
+            pq.push(_input_int);
+            break;
+        } 
+    } 
+
+    return 0;
+}
