c66a5d47401f1ce8931f0149c94332fdf23c7dcd
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-10-10 01:00:35 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string s;
+
+    cin >> s;
+    cout << s.length();
+   
+    return 0;
+} 
