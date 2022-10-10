982961447ebfcadef7fa763fbe942d4ffaea79f0
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-10-11 01:57:13 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n1,n2;
+
+    cin >> n1>>n2;
+    cout << n1*(n2-1)+1;
+   
+    return 0;
+} 
