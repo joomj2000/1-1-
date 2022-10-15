0a47b5a66e0af9eac94d5adc5d8d7950ec786d3d
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-10-16 00:00:44 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int x,y;
+    
+
+    cin >> x>>y;
+    cout << y * 2 - x;
+    
+
+   
+   
+    return 0;
+} 
