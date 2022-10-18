1cc1a7093b57afd66803db89fefc78095149f568
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-10-19 01:06:18 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int k;
+    cin >> k;
+    cout << k - 1946;
+    
+   
+   
+    return 0;
+} 
