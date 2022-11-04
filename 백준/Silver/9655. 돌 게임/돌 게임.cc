be800505ba46b2e4aaf61d37bd9458c375ed9f42
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-11-04 19:45:21 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<vector>
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+   
+    cin >> N;
+   if(N%2==0)cout <<"CY";
+   else cout << "SK";
+
+    return 0;
+} 
