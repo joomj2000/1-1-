6ca4f81639b38b8b5f8b36ec11b0b2a43a851e10
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-11-15 23:20:11 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<vector>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N,M;
+    cin >> N>>M;
+    cout << N * M - 1;
+    return 0;
+} 
