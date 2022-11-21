029fd44f7f4e4448867f40c72a17409662a0e28f
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-11-22 02:11:10 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 16 ms, Memory: 2284 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.cc
@@ -0,0 +1,53 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main() {
+	stack<int> S;
+	int n,t;
+	//cin >> n;
+	scanf("%d", &n);
+	S.push(-1);
+	long long num = 0;
+	
+	for (int i = 1;i <= n;++i) {
+		//cin >> t;
+		scanf("%d", &t);
+
+		while (S.empty() == false&&S.top() <= t) {
+			//if (S.empty())break;
+			//else 
+			S.pop();
+			//if (S.empty())break;
+		}
+		//if (S.empty()||S.top()>t)
+			S.push(t);
+		//if(S.empty()==false)
+			num += (S.size()-1);
+		//if (S.empty() || S.top().high < t) printf("0 ");
+
+		//else cout << S.top().num<<" ";
+		//else printf("%d ", S.top().num);
+		
+		
+		
+		
+	}
+	cout << num;
+
+
+
+	/*
+	6
+1
+2
+3
+4
+5
+7
+*/
+	
+}
