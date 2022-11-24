9bb989e2ddc71a46294fd58b9fb229b45fd883d6
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2022-11-24 16:46:57 +0900

    [Bronze III] Title: 2루수 이름이 뭐야, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/17350. 2루수 이름이 뭐야/2루수 이름이 뭐야.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include<vector>
+#include <cmath>
+#include <list>
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int N;
+    string str;
+    
+
+    cin >> N;
+    while (N--) {
+        cin >> str;
+        if (str.compare("anj") == 0) { cout << "뭐야;";return 0; }
+   }
+    cout << "뭐야?";
+} 
