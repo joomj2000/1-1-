4785c9a2a3143ddbb62b549ed24dd6657ddc20f5
MINJI JOO <81468129+joomj2000@users.noreply.github.com>
2023-01-03 15:32:39 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 40 ms, Memory: 30644 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.py
@@ -0,0 +1,14 @@
+N=int(input())
+
+
+for i in range(1,N+1):
+    for j in range(N-i):
+        print(' ',end='')
+    for j in range(i*2-1):
+        print('*',end='')
+    print('')
+
+        
+    
+
+    
